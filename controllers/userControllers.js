c47aad8108c84ba059b251f3bc88f58de46761eb
Abdul Rahman <mrrehman002@gmail.com>
2024-12-22 16:06:52 +0500

    fn name changed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -166,5 +166,6 @@ module.exports = {
     registerUser,
     loginUser,
     updateProfile,
-    fetchUserProfile
+    fetchUserProfile,
+    fetchUser
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser, updateProfile, fetchUserProfile } = require('../controllers/userControllers');
+const { registerUser, loginUser, updateProfile, fetchUserProfile, fetchUser } = require('../controllers/userControllers');
 const authVerify = require('../middlewares/auth');
 const userRouter = express.Router();
 
@@ -7,7 +7,7 @@ const userRouter = express.Router();
 userRouter.post('/register', registerUser);
 userRouter.post('/login', loginUser);
 userRouter.get('/profile/:id', fetchUserProfile);
-userRouter.get('/me',authVerify, fetchUserProfile);
+userRouter.get('/me',authVerify, fetchUser);
 userRouter.put('/updateProfile',authVerify, updateProfile);
 
 module.exports = userRouter;
